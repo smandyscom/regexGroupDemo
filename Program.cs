79c5a0db10b10b38641d718e15b6bf5ffe6a48cf
smandyscom <smandyscom@gmail.com>
2017-07-16 22:44:58 +0800

    group verified

--- Program.cs
+++ Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Text.RegularExpressions;
+using System.Collections.Generic;
 
 namespace regexDemo
 {
@@ -7,19 +8,31 @@ namespace regexDemo
     {
         static void Main(string[] args)
         {
-            String __input = "Model:M1 Coordinates: X9.2 Y5.2 X5.1 Y4.3 ";
+            String __input = 
+            "Model:M1 Coordinates: X1.62 Y1.01 X9.53 Y2.3 X4.94 Y2.1 X8.55 Y3.15 X2.56 Y1.25 X5.57 Y2.72 ";
+
             String __pattern = "Model:[\\w]+[\\s]+Coordinates:[\\s]+(?<coord>X(?<X>[-+]?[0-9]*\\.?[0-9]+)[\\s]*Y(?<Y>[-+]?[0-9]*\\.?[0-9]+)[\\s]*)+";
+            List<float[]> __coords = new List<float[]>();
 
             Match __match = Regex.Match(__input,
             __pattern);
 
             //now we get the "coord" group
-            foreach (Capture item in __match.Groups["coord"].Captures)
+            for (int i = 0; i < __match.Groups["coord"].Captures.Count; i++)
             {
-                Console.WriteLine(item.Value);
+                // use X,Y group restore each coordinates
+                float[] eachCoord = new float[]{0,0}; 
+               eachCoord[0] = float.Parse(__match.Groups["X"].Captures[i].Value);
+               eachCoord[1] = float.Parse(__match.Groups["Y"].Captures[i].Value);
+               __coords.Add(eachCoord);
             }
-            
-            Console.WriteLine("Hello World!");
+            //check all coordinates
+            foreach (float[] item in __coords)
+            {
+                Console.WriteLine(String.Format("{0},{1}",
+                item[0],
+                item[1]));
+            }
         }
     }
 }

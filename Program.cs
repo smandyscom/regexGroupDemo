b0cc811f7b74632367a8cb4238609ae5822652a5
smandyscom <smandyscom@gmail.com>
2017-07-16 16:12:22 +0800

    Added group parameter

--- Program.cs
+++ Program.cs
@@ -8,11 +8,16 @@ namespace regexDemo
         static void Main(string[] args)
         {
             String __input = "Model:M1 Coordinates: X9.2 Y5.2 X5.1 Y4.3 ";
-            String __pattern = "Model:[\\w]+[\\s]+Coordinates:[\\s]+(X([-+]?[0-9]*\\.?[0-9]+)[\\s]*Y([-+]?[0-9]*\\.?[0-9]+)[\\s]*)+";
+            String __pattern = "Model:[\\w]+[\\s]+Coordinates:[\\s]+(?<coord>X(?<X>[-+]?[0-9]*\\.?[0-9]+)[\\s]*Y(?<Y>[-+]?[0-9]*\\.?[0-9]+)[\\s]*)+";
 
             Match __match = Regex.Match(__input,
             __pattern);
 
+            //now we get the "coord" group
+            foreach (Capture item in __match.Groups["coord"].Captures)
+            {
+                Console.WriteLine(item.Value);
+            }
             
             Console.WriteLine("Hello World!");
         }
